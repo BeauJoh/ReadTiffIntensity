19492b0682beaced536fd0ec065309db614ce5b6
BeauJoh <beau@inbeta.org>
2011-05-01 21:39:28 +1000

    OK still experimenting with git client, here is my source thus far for image reading/writing

---
+++ Source/main.cpp
@@ -0,0 +1,34 @@
+
+#include "io_png.h"
+#include "io_tiff.h"
+
+int main(int argc, char **argv)
+{
+//    if (argc != 3)
+//        abort_("Usage: program_name <file_in> <file_out>");
+//    
+//    read_png_file(argv[1]);
+//    process_file();
+//    write_png_file(argv[2]);
+//    
+    //system("pwd");
+    //printf("png component of pixel format is sizeof (unsigned char) is %lu * 4", sizeof(unsigned char));
+    //printf("tiff pixel format is sizeof (unsigned int (uint32)) is %lu * 4", sizeof(unsigned int));
+    //printf("tiff pixel format is sizeof (unsigned int16 (uint16)) is %lu * 1", sizeof(unsigned short));
+
+//    read_png_file((char*)"toucan.png");
+//    process_file();
+//    write_png_file((char*)"toucan-negative.png");
+//    read_tiff_strip_file((char*)"Tritsmfin-s1051.tif");
+
+    //unsigned char* img = read_tiff_rgb_8_bit_from_file((char*)"MARBLES.TIF");
+    //write_tiff_rgb_8_bit_from_file((char*)"MARBLESOUT.TIF", img);
+    uint16 * img = read_tiff_strip_file((char*)"GMARBLES.TIF");
+
+    //uint16* normalizedImg = normalizeData(img);
+    //unsigned short* unnormalizedImg = unNormalizeData(normalizedImg);
+    
+    write_tiff_strip_file((char*)"GMARBLESOUT.TIF", img);
+    
+    return 0;
+}
