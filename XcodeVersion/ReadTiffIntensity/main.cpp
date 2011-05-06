5394ceac8d03725177c7cf4338719c36c70e7501
BeauJoh <beau@inbeta.org>
2011-05-06 17:50:56 +1000

    Broke Xcode, care to fix danny while I'm at badminton?

---
+++ XcodeVersion/ReadTiffIntensity/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ReadTiffIntensity
+//
+//  Created by Beau Johnston on 6/05/11.
+//  Copyright 2011 University Of New England. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
